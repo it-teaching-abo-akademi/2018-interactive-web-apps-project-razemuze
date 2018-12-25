00dcda8160ed34a7e9f1bf60d523de37cc68584b
razemuze <rasmusk97@hotmail.com>
2018-12-25 14:36:54 +0200

    Updates

---
+++ precache-manifest.93ff5d4cec036a02f30805be95e89ae6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/2018-interactive-web-apps-project-razemuze/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "5c63800509e81645a4fd",
+    "url": "/2018-interactive-web-apps-project-razemuze/static/js/runtime~main.5c638005.js"
+  },
+  {
+    "revision": "daaac97e5a227b475a86",
+    "url": "/2018-interactive-web-apps-project-razemuze/static/js/main.daaac97e.chunk.js"
+  },
+  {
+    "revision": "30b8c6ccba9e712449b1",
+    "url": "/2018-interactive-web-apps-project-razemuze/static/js/1.30b8c6cc.chunk.js"
+  },
+  {
+    "revision": "daaac97e5a227b475a86",
+    "url": "/2018-interactive-web-apps-project-razemuze/static/css/main.5806d09a.chunk.css"
+  },
+  {
+    "revision": "a805fd3637abbaa9cc78810e1a9d2bbe",
+    "url": "/2018-interactive-web-apps-project-razemuze/index.html"
+  }
+];
